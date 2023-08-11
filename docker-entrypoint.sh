86a7a0e1703d4b70ffc9f3aa503a3a2b5c92a0a1
Michael Kane Juncker <michael.juncker@enote.com>
2023-08-11 09:09:31 +0200

    chore(docker): start the service with the entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,4 +13,5 @@ done
 
 echo "Mysql is up - executing command"
 
-exec "$@"
+cd /usr/src/app/
+exec node bin/index.js -h $DATABASE_HOST -p $DATABASE_PASSWORD -d $DATABASE_NAME -u $DATABASE_USER -n 80 -r 0.0.0.0
